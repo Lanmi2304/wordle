3fd3f70b902b648b64e2960909e2db7c508f4862
Milan <107366981+Lanmi2304@users.noreply.github.com>
2025-06-08 15:23:56 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Game } from "./game";
 
 export default function Home() {
   return (
-    <div className="flex h-fit w-full items-center justify-center p-4">
+    <div className="flex h-fit w-full items-center justify-center pt-10 px-4">
       <Game />
     </div>
   );
